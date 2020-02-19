f5f990c45d0eefc8dda99014d2d5aae2ae6327f3
Glade David Brimhall <56045229+GladeBrimhall@users.noreply.github.com>
2020-02-19 13:47:07 -0700

    Setup the express app
    Created index.js that runs the app
    created layout.pug and home.pug
    created home.js route
    added port to work on localhost:3000
    added images to images folder
    made style.css
    got expreess added to the index.js
    added pug and set it to be view engine.

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Bear Claw Recipe Forum' });
+});
+
+module.exports = router;
