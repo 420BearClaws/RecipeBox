73be52d78a1d593b93303a097e8d98d67161a11b
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2020-02-19 20:55:55 -0700

    Create myrecipes.js

---
+++ routes/myrecipes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/myrecipes', function(req, res, next) {
+  res.render('myrecipes', { title: 'My Recipes' });
+});
+
+module.exports = router;
