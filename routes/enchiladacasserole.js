0d66864fe60a434270aa10627cd8f50bbcde280f
Kat <56137543+kvlee652@users.noreply.github.com>
2020-02-23 22:22:34 -0700

    delete file

--- routes/enchiladacasserole.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/enchiladacasserole', function(req, res, next) {
-  res.render('enchiladacasserole', { title: 'Green Chile Chicken Enchilada Casserole' });
-});
-
-module.exports = router;
