c503bf991c3ff9d4fddc0a120013736610c13e47
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2020-03-03 17:31:11 -0700

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Bear Claw Recipe Forum' });
+router.get("/home", function(req, res, next) {
+  res.render("home", { title: 'Bear Claw Recipe Forum' });
 });
+
 
 module.exports = router;
