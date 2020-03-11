8be7d5df25754d8481558c87baf171166417bb20
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2020-03-10 21:22:19 -0600

    Create recipes.js

---
+++ public/javascript/recipes.js
@@ -0,0 +1,17 @@
+const delete_recipe = (event) => {
+    const sender = new window.XMLHttpRequest()
+    const id = {
+        id: 'event.target.id'
+    };
+
+    sender.open('POST', 'http://localhost:3000/myrecipes', true)
+    sender.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    sender.send(JSON.stringify(tester));
+    console.log(event.target.id)
+}
+
+const delButton = document.getElementsByClassName('delete');
+for (i = 0; i < delButton.length; i++) {
+    delButton[i].addEventListener('click', delete_recipe);
+}
+
