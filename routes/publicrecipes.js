18da2d4c6d483e9a8cd347e46eb99497e26f1607
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2020-03-13 14:08:45 -0600

    Update publicrecipes.js

--- routes/publicrecipes.js
+++ routes/publicrecipes.js
@@ -6,4 +6,15 @@ router.get('/publicrecipes', function(req, res, next) {
   res.render('publicrecipes', { title: 'Public Recipes' });
 });
 
+function find_all_recipes() {
+  userrecipes.find({isPublic}, (err, userrecipes) => {
+    if (isPublic = true) {
+      return res.render("userrecipe", {userrecipes});
+    }
+    else {
+      return console.log("Error:", err);
+    };
+  })
+};
+
 module.exports = router;
